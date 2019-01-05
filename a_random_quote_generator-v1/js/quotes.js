cf5a1b4581e71ffd9cde348218a340860ab7fcf4
jordantcarlisle <jordantcarlisle@gmail.com>
2019-01-04 19:54:54 -0600

    adding initial project files

---
+++ a_random_quote_generator-v1/js/quotes.js
@@ -0,0 +1,16 @@
+
+
+var quotes = [
+{
+  quote: '',
+  source:'',
+  citation:'',
+  year:''
+}
+
+];
+
+/* References
+ES5 vs ES6 syntax - https://medium.freecodecamp.org/when-and-why-you-should-use-es6-arrow-functions-and-when-you-shouldnt-3d851d7f0b26
+
+*/
