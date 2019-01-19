171ac9572ac9c5239f9792ae00a0c9cbaeef91d4
jordantcarlisle <jordantcarlisle@gmail.com>
2019-01-19 15:15:01 -0600

    making a little progress. I will begin to rewrite most of this after some review.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -1,22 +1,52 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 1 - A Random Quote Generator
+By Jordan T. Carlisle
+http://jordancarlisle.com
+https://twitter.com/jordantcarlise
+https://github.com/jordantcarlise
 ******************************************/
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
+// This is an object that stores arrays with quotes.]
+var quotes = [
+{
+  quote: 'I made the internet',
+  source:'Al Gore',
+  citation:'Britanica',
+  year:'2000'
+},
+{
+  quote: "I'm Bob Dole",
+  source:'Bob Dole',
+  citation:'The Internet',
+  year:'1999'
+},
 
-  Recommended:
-    - Add at least one `year` and/or `citation` property to at least one
-      quote object.
-***/
+{
+  quote: 'Health care and education, in my view, are next up for fundamental software-based transformation.',
+  source:'Marc Andreessen',
+  citation:'Brainy Quotes',
+  year:'2019'
+},
 
+{
+  quote: "I'm Bob Dole",
+  source:'Bob Dole',
+  citation:'The Internet',
+  year:'1999'
+},
 
+{
+  quote: 'The Internet Made Me',
+  source:'Andrew Swartz',
+  citation:'Wikipedia',
+  year:'2006'
+}
+
+];
 
 
 /***
@@ -26,7 +56,14 @@ FSJS project 1 - A Random Quote Generator
      `quotes` array.
 ***/
 
-
+function getRandomQuote(quotes) {
+// generate a random number between 0 and the last index in the array parameter
+let randomNumber = Math.ceil(Math.random() * 2);
+// use the random number and box notation to grab a random item from the array
+quotes [randomNumber];
+// return the random item
+return.getRandomQuote();
+};
 
 
 /***
@@ -39,7 +76,34 @@ FSJS project 1 - A Random Quote Generator
    - set the `innerHTML` of the `quote-box` div to the HTML string.
 ***/
 
+function printQuote() {
+// create a variable that calls the getRandomQuote() function, passing in the quotes array as an argument
+let randomQuote = getRandomQuote (quotes);
+// create a variable that initiates your HTML string
 
+if (quote.citation && quote.year = true ) print = '<p class="quote">'+ quotes.quote +'</p>'
+'<p class="source">'+ quotes.source + '<span class="citation"> ' + quotes.citation + '</span><span class="year">' + quotes.year + '</span></p>'
+
+// using the template in the project instructions, add the two default quote properties
+// if there is a quote.citation property, add it the string
+// if there is a quote.year property, add it the string
+// close the string with the necessary closing HTML tags
+// set the innnerHTML of the .quote-box to the complete HTML string
+}
+
+function printQuote() {
+  getRandomQuote();
+
+  <p class="quote"> [quote here] </p>
+  <p class="source"> [source here]
+  <span class="citation"> [citation here] </span>
+  <span class="year"> [year here] </span>
+</p>
+}
+
+//I need to figure out how to print based on the random number and reference to the the object store.
+
+document.getElementById('quote-box').innerHTML = stringOfQuoteProperties;
 
 
 /***
